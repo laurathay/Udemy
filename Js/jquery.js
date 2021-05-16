2d959b459f7d332754ecf0f6433a8f5803a2d5f2
Laura THAY <laura.thay@ipag.fr>
2021-05-16 14:17:02 +0200

    dropdown menu loading

---
+++ Js/jquery.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    alert(loaded);
+    
+    //difference between jquery and vanilla
+    var el = document.getElementById('text');
+    console.log('In Vanilla Js :', el);
+    console.log('In JQuery : ', + $(el));
+    
+    //innerHTML
+    document.getElementById('text').innerHTML="This is my text";
+    $('#text').html('This is now my text');
+    
+    document.getElementsByClassName('my-input')[0].value = "Value of input"; //si on marque pas l'index ca marchera pas pcq avec my input ca va afficher un tableau 
+    $('input').val("Value of input");
+
+    //events 
+    $('go-button').on ('click', function() {
+        alert("Good job pressing the button!");
+    }); 
+
+    $('go-button').on ('hover', function() {
+        alert("Good job pressing the button!");
+    }); 
+
+
+    //make the dropdown menu 
+
+
+
+
+});
